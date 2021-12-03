f57b9fb883fb68e1d8bb4a0d46a07d973f8824bb
calvinyusno2359 <calvin.yusnoveri@2359media.com>
2021-12-03 18:39:30 +0800

    add: material ui icon, align button

--- src/modules/SearchBar.js
+++ src/modules/SearchBar.js
@@ -1,7 +1,19 @@
 import React from "react";
 
+import SearchIcon from "@material-ui/icons/Search";
+
+import "./SearchBar.css";
+
 function SearchBar() {
-  return <div className="SearchBar"></div>;
+  return (
+    <div className="SearchBar">
+      <input />
+      <button id="search">
+        <SearchIcon className="SearchBar__searchIcon" />
+        Search
+      </button>
+    </div>
+  );
 }
 
 export default SearchBar;
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,14 +1,14 @@
 import React from "react";
+import SearchBar from "../modules/SearchBar";
 
 import "./LandingPage.css";
 
 function LandingPage() {
   return (
     <div className="LandingPage">
-      <h1>Landing Page</h1>
       <div className="LandingPage__header">
-        <div className="LandingPage__SearchBarContainer">
-          {/* SearchBar Component */}
+        <div className="LandingPage__SearchBar">
+          <SearchBar />
         </div>
       </div>
       <div className="LandingPage__body">
