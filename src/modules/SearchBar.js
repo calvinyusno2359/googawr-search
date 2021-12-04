bd312e73ce6542d08587ae2564ce14179704d589
calvinyusno2359 <calvin.yusnoveri@2359media.com>
2021-12-04 15:04:26 +0800

    add: button onClick search function, and useState to capture input

--- src/modules/SearchBar.js
+++ src/modules/SearchBar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import SearchIcon from "@material-ui/icons/Search";
 import { Button } from "@material-ui/core";
@@ -6,10 +6,20 @@ import { Button } from "@material-ui/core";
 import "./SearchBar.css";
 
 function SearchBar() {
+  const [searchInput, setSearchInput] = useState("");
+
+  const search = (e) => {
+    e.preventDefault();
+    console.log("Search button clicked.");
+  };
+
   return (
     <div className="SearchBar">
-      <input />
-      <Button variant="outlined">
+      <input
+        value={searchInput}
+        onChange={(e) => setSearchInput(e.target.value)}
+      />
+      <Button onClick={search} variant="outlined">
         <SearchIcon className="SearchBar__searchIcon" />
         Search
       </Button>
