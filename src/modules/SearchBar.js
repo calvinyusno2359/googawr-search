9fc8eca180bdbe95d565fc34b4f57bcbe1e387d6
calvinyusno2359 <calvin.yusnoveri@2359media.com>
2021-12-04 15:25:11 +0800

    add: react router dom navigation to results page

--- src/modules/SearchBar.js
+++ src/modules/SearchBar.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 import SearchIcon from "@material-ui/icons/Search";
 import { Button } from "@material-ui/core";
@@ -8,11 +9,13 @@ import "./SearchBar.css";
 function SearchBar() {
   const emptyString = "";
   const [searchInput, setSearchInput] = useState(emptyString);
+  const navigate = useNavigate();
 
   const search = (e) => {
     e.preventDefault();
     if (searchInput === emptyString) return;
     console.log(`Search button clicked. Search input is ${searchInput}`);
+    navigate("/results"); // to ResultsPage at /results
   };
 
   return (
