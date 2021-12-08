9f13a59e95d859327ff17f876e9c8a08e7c0f52e
calvinyusno2359 <calvin.yusnoveri@2359media.com>
2021-12-08 17:59:24 +0800

    done with styling

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
+import "./index.css";
 
 import reducer, { initialSearchTerm } from "./modules/reducer";
 import { SearchTermProvider } from "./modules/SearchTermProvider";
